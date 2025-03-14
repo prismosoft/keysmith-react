2ac498e8fac14b71c73cf967c8f87fb13ee2a28e
deemonic <michael.deeming90@gmail.com>
2025-03-14 10:46:47 +0000

    removed unused code from provider

--- src/KeysmithReactServiceProvider.php
+++ src/KeysmithReactServiceProvider.php
@@ -5,7 +5,6 @@ namespace Blaspsoft\KeysmithReact;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\App;
 
 class KeysmithReactServiceProvider extends ServiceProvider
 {
@@ -14,12 +13,10 @@ class KeysmithReactServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!$this->app->routesAreCached()) {
-            Route::middleware(['web', 'auth'])
-                ->group(function () {
-                    require __DIR__.'/../routes/web.php';
-                });
-        }
+        Route::middleware(['web', 'auth'])
+            ->group(function () {
+                require __DIR__.'/../routes/web.php';
+            });
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
@@ -40,21 +37,6 @@ class KeysmithReactServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $filesystem = new Filesystem();
-
-        // Dynamically bind the correct controller based on its presence
-        $controllers = [
-            'App\Http\Controllers\APITokens\TokenController' => app_path('Http/Controllers/APITokens/TokenController.php'),
-        ];
-
-        foreach ($controllers as $class => $path) {
-            if ($filesystem->exists($path)) {
-                $this->app->bind($class);
-                break;
-            }
-        }
-        
-        // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'keysmith');
 
         return [Console\InstallCommand::class];
